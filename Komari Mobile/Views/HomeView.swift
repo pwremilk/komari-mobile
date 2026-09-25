2056a89179b15abb58f489e72efd7e70b8c11a7f
hermes <hermes@local>
2026-09-25 09:59:26 +0800

    ios16: explicit keypaths for kmBinding

--- Komari Mobile/Views/HomeView.swift
+++ Komari Mobile/Views/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
 
     var body: some View {
         if #available(iOS 18.0, *) {
-            TabView(selection: kmBinding(for: .tab, on: state)) {
+            TabView(selection: kmBinding(for: KMState.tab, on: state)) {
                 Tab(value: MainTab.servers) {
                     ServerListView()
                 } label: {
@@ -27,7 +27,7 @@ struct HomeView: View {
             }
         }
         else {
-            TabView(selection: kmBinding(for: .tab, on: state)) {
+            TabView(selection: kmBinding(for: KMState.tab, on: state)) {
                 ServerListView()
                     .tabItem {
                         Label(MainTab.servers.title, systemImage: MainTab.servers.systemName)
